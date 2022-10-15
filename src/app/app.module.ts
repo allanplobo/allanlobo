b2dc59d087ce2649d725055fd29b335b06e2c3de
Allan Lobo <allanplobo@gmail.com>
2022-10-15 18:39:42 -0300

    feat: init topbar construction and the base of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingPageComponent
+    LandingPageComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,

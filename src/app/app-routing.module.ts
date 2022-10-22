da364ad52b60d81f0a55dd59cb969142c6498b71
Allan Lobo <allanplobo@gmail.com>
2022-10-22 16:02:20 -0300

    feat: add all pages components and new sass utilities

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AboutComponent } from "./pages/about/about.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { LandingPageComponent } from "./pages/landing-page/landing-page.component";
@@ -5,7 +6,21 @@ import { LandingPageComponent } from "./pages/landing-page/landing-page.componen
 const routes: Routes = [
   {
     path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
     component: LandingPageComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "blog",
+    loadChildren: () =>
+      import("./pages/blog/blog.module").then((m) => m.BlogModule),
   },
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'allanlobo';
-
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang(translate.getBrowserLang() || "pt");
+  }
 }
